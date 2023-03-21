de52e4dde62e15882d4039ac010a29ce39b6753b
witorsather <sathews@gmail.com>
2023-03-21 19:47:03 -0300

    update login.

--- angular-gpp/src/app/components/login/login.component.ts
+++ angular-gpp/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/models/LoginModel';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,17 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  username: string = "";
+  password: string = "";
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public login:LoginModel = new LoginModel();
+
+  logar(){
   }
 
 }
