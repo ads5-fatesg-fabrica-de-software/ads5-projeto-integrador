0b3f12ebe8ca7f4b1b43265cf7d13f4e497a9cfc
witorsather <sathews@gmail.com>
2023-03-21 15:06:25 -0300

    login

--- angular-gpp/src/app/app-routing.module.ts
+++ angular-gpp/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PecaListComponent } from './components/peca-list/peca-list.component';
 import { PecaComponent } from './components/peca/peca.component';
 import { EntregaComponent } from './components/entrega/entrega.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path: '', component: PecaComponent},
+  {path: '', component: LoginComponent},
   {path: 'peca/:id', component: PecaComponent},
   {path: 'pecaList', component: PecaListComponent},
   {path: 'entrega', component: EntregaComponent},
