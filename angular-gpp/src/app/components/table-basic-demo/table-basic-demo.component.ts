243d57ce992fa973e19491787293d82a7ec2a651
witorsather <sathews@gmail.com>
2023-04-12 22:01:10 -0300

    table basic inicio.

--- angular-gpp/src/app/components/table-basic-demo/table-basic-demo.component.ts
+++ angular-gpp/src/app/components/table-basic-demo/table-basic-demo.component.ts
@@ -11,27 +11,40 @@ import { PecaService } from 'src/app/services/peca.service';
 })
 export class TableBasicDemoComponent implements OnInit {
 
-
   public source: LocalDataSource = new LocalDataSource();
 
+  onRowEditInit(_t14: any) {
+    throw new Error('Method not implemented.');
+  }
+  onRowEditSave(_t14: any) {
+    throw new Error('Method not implemented.');
+  }
+  onRowEditCancel(_t14: any, arg1: any) {
+    throw new Error('Method not implemented.');
+  }
 
-    products: PecaModel[] = [];
 
-    constructor(private pecaService: PecaService, private router: Router){}
 
-    ngOnInit(): void {
-      this.pecaService.list().subscribe(resp => {
-        this.source.load(resp);
-      });
-    }
-  
-    novo(){
-      this.router.navigateByUrl('/peca/novo');
-    }
-  
-    onCustomAction(event: any) {
-      let peca:PecaModel = event.data;
-      console.log(event);
-      this.router.navigate([`peca/${peca.idPeca}`]);
-    }  
+
+  peca: PecaModel[] = [];
+  editing: any;
+  ri: any;
+
+  constructor(private pecaService: PecaService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.pecaService.list().subscribe(resp => {
+      this.source.load(resp);
+    });
+  }
+
+  novo() {
+    this.router.navigateByUrl('/peca/novo');
+  }
+
+  onCustomAction(event: any) {
+    let peca: PecaModel = event.data;
+    console.log(event);
+    this.router.navigate([`peca/${peca.idPeca}`]);
+  }
 }
