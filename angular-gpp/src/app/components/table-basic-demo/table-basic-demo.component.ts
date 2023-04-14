a44b1ea780b2acface59c3d583621faaaa2b70a0
witorsather <sathews@gmail.com>
2023-04-14 16:41:52 -0300

    tentativa de criar uma busca.

--- angular-gpp/src/app/components/table-basic-demo/table-basic-demo.component.ts
+++ angular-gpp/src/app/components/table-basic-demo/table-basic-demo.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalDataSource } from 'ng2-smart-table';
 import { PecaModel } from 'src/app/models/PecaModel';
 import { PecaService } from 'src/app/services/peca.service';
-
-
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-table-basic-demo',
@@ -12,14 +11,42 @@ import { PecaService } from 'src/app/services/peca.service';
   styleUrls: ['./table-basic-demo.component.css']
 })
 export class TableBasicDemoComponent implements OnInit {
+ 
+
+ 
 
 
-  public source: LocalDataSource = new LocalDataSource();
+  
+  @ViewChild('dt2') dt2: Table | undefined;
 
-
+  ngAfterViewInit() {
+    console.log('dt2:', this.dt2);
+  }
+  
   pecas: PecaModel[] = [];
 
-  constructor(private pecaService: PecaService, private router: Router) { }
+  public source: LocalDataSource = new LocalDataSource(this.pecas);
+
+  
+  constructor(private pecaService: PecaService, private router: Router, private cdr: ChangeDetectorRef) { }
+
+  // onSearch(event: Event) {
+  //   console.log("test")
+  //   const value = (event.target as HTMLInputElement).value;
+  //   this.dt2?.filterGlobal(value, 'contains');
+  // }
+  
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    console.log('Search value:', value);
+    this.dt2?.filterGlobal(value, 'contains');
+    console.log('Filtered data:', this.dt2?.value);
+    this.cdr.detectChanges(); // trigger change detection
+
+    console.log('test data:', this.dt2?.value);
+  }
+  
+  
 
   ngOnInit(): void {
     this.list();
