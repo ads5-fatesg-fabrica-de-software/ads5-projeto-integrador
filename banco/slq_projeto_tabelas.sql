f70437b6c641686685713c5d5bbf01abedfcc558
witorsather <sathews@gmail.com>
2023-04-18 21:05:28 -0300

    sql update.

--- banco/slq_projeto_tabelas.sql
+++ banco/slq_projeto_tabelas.sql
@@ -46,4 +46,50 @@ VALUES
     ('Aperture Science'),
     ('InGen Corporation');
 
+//produto
+SELECT id_produto, descricao, fornecedor_id_fornecedor
+FROM public.produto;
+  -- Exemplo 1
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Escrivaninha de madeira maciça com gavetas embutidas', 11);
+
+-- Exemplo 2
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Guarda-roupa com três portas de correr e prateleiras internas', 12);
+
+-- Exemplo 3
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Mesa de escritório com tampo de vidro temperado e estrutura metálica', 11);
+
+-- Exemplo 4
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Cama box de casal com colchão de molas ensacadas', 12);
+
+-- Exemplo 5
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Sofá retrátil de couro sintético com três lugares', 13);
+
+-- Exemplo 6
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Geladeira frost-free de inox com dispenser de água', 14);
+
+-- Exemplo 7
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Fogão a gás com quatro bocas e acendimento automático', 14);
+
+-- Exemplo 8
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Televisão LED Full HD de 50 polegadas', 15);
+
+-- Exemplo 9
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Notebook com processador Intel Core i5 e 8GB de RAM', 16);
+
+-- Exemplo 10
+INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
+VALUES ('Smartphone Android com câmera de alta resolução e tela de 6 polegadas', 17);
+
+  
+  
+  
 
