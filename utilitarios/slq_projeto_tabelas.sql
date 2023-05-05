c8a24cc0bc82ebde7b3fa0f83f88af3b7f27dc4e
witosather <satherws@gmail.com>
2023-05-05 20:42:43 -0300

    :books: docs: Atualizaçao do sql

--- utilitarios/slq_projeto_tabelas.sql
+++ utilitarios/slq_projeto_tabelas.sql
@@ -1,88 +1,86 @@
-
---peça
-SELECT id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes
-FROM public.peca;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (1, true, 10, 'COD1', 'Blue', 20, 'Widget 1', 101, 5, 'Metal', 'Steel', 123, 2, 2, 10, 3)
-ON CONFLICT (id_peca) DO NOTHING;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (2, true, 15, 'COD2', 'Red', 25, 'Widget 2', 102, 6, 'Plastic', 'Polypropylene', 234, 3, 3, 15, 4)
-ON CONFLICT (id_peca) DO NOTHING;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (3, true, 8, 'COD3', 'Green', 15, 'Widget 3', 101, 3, 'Metal', 'Aluminum', 345, 1, 2, 5, 2)
-ON CONFLICT (id_peca) DO NOTHING;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (4, true, 12, 'COD4', 'Yellow', 30, 'Widget 4', 102, 4, 'Glass', 'Soda-lime', 456, 2, 4, 8, 3)
-ON CONFLICT (id_peca) DO NOTHING;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (5, true, 10, 'COD5', 'Purple', 40, 'Widget 5', 101, 5, 'Plastic', 'ABS', 567, 2, 2, 10, 3)
-ON CONFLICT (id_peca) DO NOTHING;
-
-INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
-VALUES (6, true, 14, 'COD6', 'Orange', 35, 'Widget 6', 102, 7, 'Metal', 'Copper', 678, 3, 3, 20, 4)
-ON CONFLICT (id_peca) DO NOTHING;
-
---****************************************************************************************************************************************************************************************************************************
 --fornecedor
+
+
+
+TRUNCATE TABLE fornecedor CASCADE;
+
+INSERT INTO fornecedor (id_fornecedor, nome_fornecedor)
+VALUES
+    (1, 'Acme Corporation'),
+    (2, 'Globex Corporation'),
+    (3, 'Stark Industries')
+ON CONFLICT (id_fornecedor) DO NOTHING;
+
+
 SELECT id_fornecedor, nome_fornecedor
 FROM public.fornecedor;
 
-INSERT INTO public.fornecedor (nome_fornecedor)
-VALUES
-    ('Acme Corporation'),
-    ('Globex Corporation'),
-    ('Stark Industries'),
-    ('Wayne Enterprises'),
-    ('Umbrella Corporation'),
-    ('Weyland-Yutani Corporation'),
-    ('Oscorp Industries'),
-    ('Tyrell Corporation'),
-    ('Aperture Science'),
-    ('InGen Corporation');
-
 --****************************************************************************************************************************************************************************************************************************
 --produto
+
+
+TRUNCATE TABLE produto CASCADE;
+
+INSERT INTO produto (id_produto, descricao, fornecedor_id_fornecedor) 
+VALUES 
+    (1, 'Escrivaninha de madeira maciça com gavetas embutidas', 1),
+    (2, 'Guarda-roupa com três portas de correr e prateleiras internas', 2),
+    (3, 'Mesa de escritório com tampo de vidro temperado e estrutura metálica', 3)
+ON CONFLICT (id_produto) DO NOTHING;
+
 SELECT id_produto, descricao, fornecedor_id_fornecedor
-FROM public.produto;
+FROM produto;
 
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Escrivaninha de madeira maciça com gavetas embutidas', 11);
+--****************************************************************************************************************************************************************************************************************************
+--cliente
 
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Guarda-roupa com três portas de correr e prateleiras internas', 12);
 
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Mesa de escritório com tampo de vidro temperado e estrutura metálica', 11);
+TRUNCATE TABLE cliente CASCADE;
 
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Cama box de casal com colchão de molas ensacadas', 12);
+INSERT INTO cliente (id_cliente, cpf_cnpj, email, nome)
+VALUES 
+    (1, '123.456.789-00', 'john@example.com', 'John Doe'),
+    (2, '987.654.321-00', 'jane@example.com', 'Jane Smith'),
+    (3, '555.555.555-55', 'bob@example.com', 'Bob Johnson')
+ON CONFLICT (id_cliente) DO NOTHING;
 
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Sofá retrátil de couro sintético com três lugares', 13);
-
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Geladeira frost-free de inox com dispenser de água', 14);
-
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Fogão a gás com quatro bocas e acendimento automático', 14);
-
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Televisão LED Full HD de 50 polegadas', 15);
-
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Notebook com processador Intel Core i5 e 8GB de RAM', 16);
-
-INSERT INTO public.produto (descricao, fornecedor_id_fornecedor) 
-VALUES ('Smartphone Android com câmera de alta resolução e tela de 6 polegadas', 17);
+SELECT id_cliente, cpf_cnpj, email, nome
+FROM cliente;
 
 --****************************************************************************************************************************************************************************************************************************
 
+--peça
 
-  
+
+TRUNCATE TABLE peca CASCADE;
+
+INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
+VALUES (1, true, 10, 'COD1', 'Blue', 20, 'Widget 1', 11, 5, 'Metal', 'Steel', 123, 2, 2, 10, 3)
+ON CONFLICT (id_peca) DO NOTHING;
+
+INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
+VALUES (2, true, 15, 'COD2', 'Red', 25, 'Widget 2', 12, 6, 'Plastic', 'Polypropylene', 234, 3, 3, 15, 4)
+ON CONFLICT (id_peca) DO NOTHING;
+
+INSERT INTO public.peca (id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes)
+VALUES (3, true, 8, 'COD3', 'Green', 15, 'Widget 3', 11, 3, 'Metal', 'Aluminum', 345, 1, 2, 5, 2)
+ON CONFLICT (id_peca) DO NOTHING;
+
+SELECT id_peca, active, altura, codigo_fabrica, cor, custo, descricao, id_fornecedor, largura, material, material_fabricacao, numero, profundidade, unidade, unidade_medida, volumes
+FROM peca;
+
+--****************************************************************************************************************************************************************************************************************************
+
+TRUNCATE TABLE filial CASCADE;
+
+INSERT INTO filial (id_filial, numero_filial, sigla)
+VALUES
+    (1, 1234, 'Filial Sao Paulo'),
+    (2, 5678, 'Filial Rio de Janeiro'),
+    (3, 91011, 'Filial Bahia')
+ON CONFLICT DO NOTHING;
+
+SELECT id_filial, numero_filial, sigla
+FROM public.filial;
   
 
