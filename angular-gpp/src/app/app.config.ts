5939de3ff312b33729272c7735602b92ec70252d
witosather <satherws@gmail.com>
2023-05-15 19:43:57 -0300

    :ok_hand: style: app.config.ts

--- angular-gpp/src/app/app.config.ts
+++ angular-gpp/src/app/app.config.ts
@@ -1,2 +1,3 @@
-// export const environment.url = 'http://18.230.196.244:8081';
-export const environment.url = 'http://localhost:8081';
+// export const API_URL= 'http://localhost:8081';
+
+export const API_URL = 'http://18.230.196.244:8081';
