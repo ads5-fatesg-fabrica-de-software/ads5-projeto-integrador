14e96cbd23dae83dc47f4e91a93b2b76da6dfffb
witosather <satherws@gmail.com>
2023-05-20 06:50:00 -0300

    :bug: fix: editar peca

--- angular-gpp/src/app/components/peca-edit/peca-edit.component.ts
+++ angular-gpp/src/app/components/peca-edit/peca-edit.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { PecaModel } from 'src/app/models/PecaModel';
 import { PecaService } from '../../services/peca.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ProdutoModel } from 'src/app/models/ProdutoModel'; // Add this import
+import { ProdutoModel } from 'src/app/models/ProdutoModel';
+import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
   selector: 'app-peca-edit',
@@ -12,8 +13,15 @@ import { ProdutoModel } from 'src/app/models/ProdutoModel'; // Add this import
 export class PecaEditComponent implements OnInit {
   displayDialog: boolean = true;
   peca: PecaModel = new PecaModel();
+  fornecedores: string[] = []; // Populate this array with the list of fornecedores
+  produtos: ProdutoModel[] = [];
 
-  constructor(private pecaService: PecaService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private pecaService: PecaService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private produtoService: ProdutoService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -22,6 +30,10 @@ export class PecaEditComponent implements OnInit {
       
       this.peca = peca;
       this.peca.produto = { ...peca.produto };
+    });
+
+    this.produtoService.list().subscribe(produtos => {
+      this.produtos = produtos;
     });
   }
 
--- gpp-api/src/main/java/com/develop/gpp/controller/PecaController.java
+++ gpp-api/src/main/java/com/develop/gpp/controller/PecaController.java
@@ -1,6 +1,7 @@
 package com.develop.gpp.controller;
 
-import com.develop.gpp.domain.entity.PecaModel;
+import com.develop.gpp.domain.entity.*;
+import com.develop.gpp.domain.entity.ItemDocumentoFiscalModel;
 import com.develop.gpp.domain.entity.dto.PecaDTO;
 import com.develop.gpp.domain.service.PecaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/pecas")
@@ -46,4 +51,29 @@ public class PecaController {
     public ResponseEntity<PecaModel> buscarPorId(@PathVariable Integer id) {
         return pecaService.buscarPorId(id);
     }
+
+//    @PostConstruct
+//    public void init() {
+//
+//        PecaModel peca = new PecaModel();
+//        peca.setNumero("12345");
+//        peca.setCodigoFabrica("ABC123");
+//        peca.setUnidade(1);
+//        peca.setDescricao("Sample description");
+//        peca.setAltura(10);
+//        peca.setLargura(20);
+//        peca.setProfundidade(30);
+//        peca.setUnidadeMedida(2);
+//        peca.setVolumes("5");
+//        peca.setActive(true);
+//        peca.setCusto(100.0);
+//        peca.setCor("Red");
+//        peca.setMaterial("Metal");
+//        peca.setIdFornecedor(1L);
+//        peca.setMaterialFabricacao("Steel");
+//
+//        PecaModel savedPeca = pecaService.salvarPeca(peca);
+//
+//
+//    }
 }
