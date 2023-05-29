9acd61d87bebc8fb667c39ed3ce6f1aab1bad101
witosather <satherws@gmail.com>
2023-05-29 09:19:28 -0300

    :sparkles: feat: validadores em peça edição

--- angular-gpp/src/app/components/peca-edit/peca-edit.component.ts
+++ angular-gpp/src/app/components/peca-edit/peca-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { PecaModel } from 'src/app/models/PecaModel';
-import { PecaService } from '../../services/peca.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { PecaModel } from 'src/app/models/PecaModel';
 import { ProdutoModel } from 'src/app/models/ProdutoModel';
+import { PecaService } from '../../services/peca.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
@@ -16,21 +17,25 @@ export class PecaEditComponent implements OnInit {
   fornecedores: string[] = []; // Populate this array with the list of fornecedores
   produtos: ProdutoModel[] = [];
   pageTitle: string = 'Edição';
+  pecaForm!: FormGroup; // Add "!" to indicate that it will be initialized in the constructor
+  selectedProduto: ProdutoModel | null = null;
 
   constructor(
     private pecaService: PecaService,
     private router: Router,
     private route: ActivatedRoute,
-    private produtoService: ProdutoService
-  ) {}
+    private produtoService: ProdutoService,
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm(); // Call the buildForm method to initialize the pecaForm
+  }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pecaService.getPorIdPeca(id).subscribe(peca => {
-      // console.log(peca);
-      
       this.peca = peca;
       this.peca.produto = { ...peca.produto };
+      this.pecaForm.patchValue(this.peca);
     });
 
     this.produtoService.list().subscribe(produtos => {
@@ -39,7 +44,15 @@ export class PecaEditComponent implements OnInit {
   }
 
   public editar(): void {
+    if (this.pecaForm.invalid) {
+      return;
+    }
+
+    this.peca = { ...this.peca, ...this.pecaForm.value };
+    // this.peca.idFornecedor = this.selectedProduto?.fornecedor?.idFornecedor;
+
     this.pecaService.add(this.peca).subscribe(() => {
+      this.peca = new PecaModel();
       this.router.navigateByUrl('/pecaList');
     });
   }
@@ -51,5 +64,27 @@ export class PecaEditComponent implements OnInit {
   onDialogHide(): void {
     this.cancelar();
   }
+  getSelectedProdutoFornecedorNome(): string {
+    const selectedProduto = this.pecaForm.get('produto')?.value;
+    return selectedProduto?.fornecedor?.nomeFornecedor || '';
+  }
   
+  private buildForm(): void {
+    this.pecaForm = this.formBuilder.group({
+      numero: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      codigoFabrica: ['', Validators.required],
+      unidade: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      descricao: ['', Validators.required],
+      altura: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      largura: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      profundidade: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      unidadeMedida: ['', Validators.required],
+      volumes: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      active: [false],
+      custo: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]],
+      cor: ['', Validators.required],
+      material: ['', Validators.required],
+      produto: ['', Validators.required]
+    });
+  }
 }
