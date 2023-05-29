b7b9011d2bdf6845ad0ee57d2fa75f598e30ee73
witosather <satherws@gmail.com>
2023-05-29 14:24:10 -0300

    :sparkles: feat: validadores em forneced form

--- angular-gpp/src/app/components/fornecedor-table/fornecedor-table.component.ts
+++ angular-gpp/src/app/components/fornecedor-table/fornecedor-table.component.ts
@@ -11,79 +11,70 @@ import { FornecedorModel } from 'src/app/models/FornecedorModel';
   styleUrls: ['./fornecedor-table.component.css']
 })
 export class FornecedorTableComponent implements OnInit {
-  
-  @ViewChild('dt2') dt2: Table | undefined;
+  @ViewChild('dt2') dt2!: Table;
+
+  fornecedores: FornecedorModel[] = [];
+  source: LocalDataSource = new LocalDataSource(this.fornecedores);
+  pageTitle = 'Fornecedor';
+
+  constructor(
+    private fornecedorService: FornecedorService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngAfterViewInit() {
     // console.log('dt2:', this.dt2);
   }
-  
-  fornecedores: FornecedorModel[] = [];
-
-  public source: LocalDataSource = new LocalDataSource(this.fornecedores);
-
-  
-  constructor(private fornecedorService: FornecedorService, private router: Router, private cdr: ChangeDetectorRef) { }
-
-  // onSearch(event: Event) {
-  //   console.log("test")
-  //   const value = (event.target as HTMLInputElement).value;
-  //   this.dt2?.filterGlobal(value, 'contains');
-  // }
-  
-  onSearch(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
-    // console.log('Search value:', value);
-    this.dt2?.filterGlobal(value, 'contains');
-    // console.log('Filtered data:', this.dt2?.value);
-    this.cdr.detectChanges(); // trigger change detection
-
-    // console.log('test data:', this.dt2?.value);
-  }
-  
-  
 
   ngOnInit(): void {
     this.list();
   }
 
-  list(){
+  list(): void {
     this.fornecedorService.list().subscribe(resp => {
       this.fornecedores = resp;
     });
   }
 
-  novo() {
+  novo(): void {
     this.router.navigateByUrl('/fornecedor/novo');
   }
 
-  onCustomAction(event: any) {
-    let fornecedor: FornecedorModel = event.data;
+  onCustomAction(event: any): void {
+    const fornecedor: FornecedorModel = event.data;
     console.log(event);
     this.router.navigate([`fornecedor/${fornecedor.idFornecedor}`]);
   }
 
-
-
-  editeFornecedor(fornecedor: FornecedorModel) {
+  editeFornecedor(fornecedor: FornecedorModel): void {
     this.router.navigateByUrl(`/fornecedor/editar/${fornecedor.idFornecedor}`);
-}
+  }
 
-editFornecedor(id: number) {
-  this.router.navigate(['/fornecedor/editar', id]);
-}
+  editFornecedor(id: number): void {
+    this.router.navigate(['/fornecedor/editar', id]);
+  }
 
-
-  deleteFornecedor(id: number) {
+  deleteFornecedor(id: number): void {
     // console.log('deletar fornecedor');
-  
     this.fornecedorService.delete(id).subscribe(() => {
       this.list();
       // console.log(`Fornecedor ${id} foi deletada`);
     });
   }
+
+  onSearch(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    // console.log('Search value:', value);
+    this.dt2?.filterGlobal(value, 'contains');
+    // console.log('Filtered data:', this.dt2?.value);
+    this.cdr.detectChanges(); // trigger change detection
+    // console.log('test data:', this.dt2?.value);
+  }
+
+    adicionarFornecedor(): void {
+    this.router.navigateByUrl(`fornecedor/:id`);
+  }
+
   
-
-
-
 }
