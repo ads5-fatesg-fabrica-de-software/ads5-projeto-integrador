4774f53b6afc9bf1b1f88bd45f1c6c8faca9b9b0
witosather <satherws@gmail.com>
2023-05-30 11:12:46 -0300

    :sparkles: feat: validadores produto

--- angular-gpp/src/app/components/produto-edit/produto-edit.component.ts
+++ angular-gpp/src/app/components/produto-edit/produto-edit.component.ts
@@ -1,10 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Message, MessageService } from 'primeng/api';
 import { FornecedorModel } from 'src/app/models/FornecedorModel';
 import { ProdutoModel } from 'src/app/models/ProdutoModel';
 import { FornecedorService } from 'src/app/services/fornecedor.service';
 import { ProdutoService } from 'src/app/services/produto.service';
+
+function lettersOnly(control: AbstractControl): { [key: string]: any } | null {
+  const lettersPattern = /^[A-Za-z]+$/;
+  let value = control.value;
+
+  if (value == null || value === '') {
+    return null; // Skip validation if the value is null or empty
+  }
+
+  if (typeof value !== 'string') {
+    value = value.toString(); // Convert to string if value is not already a string
+  }
+
+  if (!lettersPattern.test(value.replace(/\s/g, '')) || value.length < 4) {
+    return { lettersOnly: true };
+  }
+  
+  return null;
+}
 
 @Component({
   selector: 'app-produto-edit',
@@ -18,8 +38,13 @@ export class ProdutoEditComponent implements OnInit {
   filteredFornecedors: FornecedorModel[] = [];
   msgs: Message[] = [];
   produtoId!: number;
+  pageTitle: string = 'Produto';
+  produtoForm: FormGroup;
+  selectedFornecedor: FornecedorModel | null = null;
 
   ngOnInit(): void {
+    this.buildForm();
+
     this.fornecedorService.list().subscribe(fornecedors => {
       this.fornecedors = fornecedors;
     });
@@ -35,8 +60,14 @@ export class ProdutoEditComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private fornecedorService: FornecedorService,
-    private messageService: MessageService
-  ) {}
+    private messageService: MessageService,
+    private formBuilder: FormBuilder
+  ) {
+    this.produtoForm = this.formBuilder.group({
+      descricao: ['', Validators.required],
+      fornecedor: [null, Validators.required] // Use 'fornecedor' as the form control name
+    });
+  }
 
   produto: ProdutoModel = {
     descricao: '',
@@ -47,17 +78,47 @@ export class ProdutoEditComponent implements OnInit {
     this.filteredFornecedors = this.fornecedors.filter(fornecedor =>
       fornecedor?.nomeFornecedor?.toLowerCase().includes(event.query.toLowerCase())
     );
+
+    this.selectedFornecedor = null;
   }
 
   loadProduto(): void {
     this.produtoService.get(this.produtoId).subscribe(produto => {
       this.produto = produto;
+
+      // Update form values with loaded product data
+      this.produtoForm.patchValue({
+        descricao: produto.descricao,
+        fornecedor: produto.fornecedor
+      });
     });
   }
 
   editar(): void {
+
+    if (this.produtoForm.invalid) {
+      return;
+    }
+
+    this.produto = { ...this.produto, ...this.produtoForm.value };
+
     this.produtoService.post(this.produto).subscribe(() => {
       this.router.navigateByUrl('/produtoList');
+    });
+  }
+
+  cancelar(): void {
+    this.router.navigateByUrl('/produtoList');
+  }
+
+  onDialogHide(): void {
+    this.cancelar();
+  }
+
+  private buildForm(): void {
+    this.produtoForm = this.formBuilder.group({
+      descricao: ['', Validators.required],
+      fornecedor: [null, Validators.required] // Use 'fornecedor' as the form control name
     });
   }
 }
--- angular-gpp/src/app/components/produto-table/produto-table.component.ts
+++ angular-gpp/src/app/components/produto-table/produto-table.component.ts
@@ -4,16 +4,18 @@ import { LocalDataSource } from 'ng2-smart-table';
 import { Table } from 'primeng/table';
 import { ProdutoService } from 'src/app/services/produto.service';
 import { ProdutoModel } from 'src/app/models/ProdutoModel';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-produto-table',
   templateUrl: './produto-table.component.html',
-  styleUrls: ['./produto-table.component.css']
+  styleUrls: ['./produto-table.component.css'],
+  providers: [MessageService]
 })
 export class ProdutoTableComponent implements OnInit {
 
   @ViewChild('dt2') dt2: Table | undefined;
-  
+
   produtos: ProdutoModel[] = [];
   pageTitle: string = 'Produto';
   public source: LocalDataSource = new LocalDataSource(this.produtos);
@@ -21,13 +23,14 @@ export class ProdutoTableComponent implements OnInit {
   constructor(
     private produtoService: ProdutoService,
     private router: Router,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private messageService: MessageService
   ) { }
 
   ngAfterViewInit() {
     // console.log('dt2:', this.dt2);
   }
-  
+
   ngOnInit(): void {
     this.list();
   }
@@ -60,7 +63,16 @@ export class ProdutoTableComponent implements OnInit {
   deleteProduto(id: number): void {
     this.produtoService.delete(id).subscribe(() => {
       this.list();
-      console.log(`Produto ${id} foi deletado`);
+      this.showMessage(`O produto ${id} foi deletado`);
+    });
+  }
+
+  showMessage(message: string): void {
+    this.messageService.add({
+      severity: 'success',
+      summary: '',
+      detail: message,
+      life: 10000
     });
   }
 
@@ -69,7 +81,7 @@ export class ProdutoTableComponent implements OnInit {
     console.log('Search value:', value);
     this.dt2?.filterGlobal(value, 'contains');
     console.log('Filtered data:', this.dt2?.value);
-    this.cdr.detectChanges(); // trigger change detection
+    this.cdr.detectChanges();
     console.log('test data:', this.dt2?.value);
   }
 
