4f1409c859eaebe5062446d6cf4038096af4601f
witosather <satherws@gmail.com>
2023-05-30 09:57:07 -0300

    :zap: perf: validadores produto

--- angular-gpp/src/app/components/produto-table/produto-table.component.ts
+++ angular-gpp/src/app/components/produto-table/produto-table.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LocalDataSource } from 'ng2-smart-table';
-import { ProdutoService } from 'src/app/services/produto.service';
 import { Table } from 'primeng/table';
+import { ProdutoService } from 'src/app/services/produto.service';
 import { ProdutoModel } from 'src/app/models/ProdutoModel';
+
 @Component({
   selector: 'app-produto-table',
   templateUrl: './produto-table.component.html',
@@ -12,77 +13,67 @@ import { ProdutoModel } from 'src/app/models/ProdutoModel';
 export class ProdutoTableComponent implements OnInit {
 
   @ViewChild('dt2') dt2: Table | undefined;
+  
+  produtos: ProdutoModel[] = [];
+  pageTitle: string = 'Produto';
+  public source: LocalDataSource = new LocalDataSource(this.produtos);
+
+  constructor(
+    private produtoService: ProdutoService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngAfterViewInit() {
     // console.log('dt2:', this.dt2);
   }
   
-  produtos: ProdutoModel[] = [];
+  ngOnInit(): void {
+    this.list();
+  }
 
-  public source: LocalDataSource = new LocalDataSource(this.produtos);
+  list(): void {
+    this.produtoService.list().subscribe(resp => {
+      this.produtos = resp;
+      this.source.load(resp);
+    });
+  }
 
-  
-  constructor(private produtoService: ProdutoService, private router: Router, private cdr: ChangeDetectorRef) { }
+  novo(): void {
+    this.router.navigateByUrl('/produto/novo');
+  }
 
-  // onSearch(event: Event) {
-  //   console.log("test")
-  //   const value = (event.target as HTMLInputElement).value;
-  //   this.dt2?.filterGlobal(value, 'contains');
-  // }
-  
-  onSearch(event: Event) {
+  onCustomAction(event: any): void {
+    const produto: ProdutoModel = event.data;
+    console.log(event);
+    this.router.navigate([`produto/${produto.idProduto}`]);
+  }
+
+  editeProduto(produto: ProdutoModel): void {
+    this.router.navigateByUrl(`/produto/editar/${produto.idProduto}`);
+  }
+
+  editProduto(id: number): void {
+    this.router.navigate(['/produto/editar', id]);
+  }
+
+  deleteProduto(id: number): void {
+    this.produtoService.delete(id).subscribe(() => {
+      this.list();
+      console.log(`Produto ${id} foi deletado`);
+    });
+  }
+
+  onSearch(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
     console.log('Search value:', value);
     this.dt2?.filterGlobal(value, 'contains');
     console.log('Filtered data:', this.dt2?.value);
     this.cdr.detectChanges(); // trigger change detection
-
     console.log('test data:', this.dt2?.value);
   }
-  
-  
 
-  ngOnInit(): void {
-    this.list();
+  adicionarProduto(): void {
+    this.router.navigateByUrl(`produto/:id`);
   }
-
-  list(){
-    this.produtoService.list().subscribe(resp => {
-      this.produtos = resp;
-    });
-  }
-
-  novo() {
-    this.router.navigateByUrl('/produto/novo');
-  }
-
-  onCustomAction(event: any) {
-    let produto: ProdutoModel = event.data;
-    console.log(event);
-    this.router.navigate([`produto/${produto.idProduto}`]);
-  }
-
-
-
-  editeProduto(produto: ProdutoModel) {
-    this.router.navigateByUrl(`/produto/editar/${produto.idProduto}`);
-}
-
-editProduto(id: number) {
-  this.router.navigate(['/produto/editar', id]);
-}
-
-
-  deleteProduto(id: number) {
-    // console.log('deletar produto');
-  
-    this.produtoService.delete(id).subscribe(() => {
-      this.list();
-      console.log(`Produto ${id} foi deletada`);
-    });
-  }
-  
-
-
-
 }
