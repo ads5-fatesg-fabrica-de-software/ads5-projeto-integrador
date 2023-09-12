fd94077b2d5aea330602cbafc28e17484b070668
wendersonads <wendersonads5.5@gmail.com>
2023-09-12 20:11:35 -0300

    :ok_hand: modelo lista

--- gpp_app/lib/domain/service/PecaService.dart
+++ gpp_app/lib/domain/service/PecaService.dart
@@ -6,16 +6,28 @@ import 'package:gpp_app/domain/models/PecaModel.dart';
 import 'package:http/http.dart';
 
 class PecaService {
-  Future<void> lista() async {
-    Response response = await post(
-      Uri.parse(Conection.url + 'pecas'),
-      headers: {'Content-Type': 'application/json'},
-      body: PecaModel,
-    );
-    if (response.statusCode == 200) {
-      Fluttertoast.showToast(msg: 'Done!');
-    } else {
-      Fluttertoast.showToast(msg: jsonDecode(response.body)['message']);
+  List<PecaModel> pecas = [];
+  bool carregando = false;
+
+  lista() async {
+    try {
+      carregando = true;
+      Response response = await get(
+        Uri.parse(Conection.url + 'pecas/'),
+        headers: {'Content-Type': 'application/json'},
+      );
+      var data = jsonDecode(response.body);
+      print(data);
+      pecas = data['pecas']
+          .map<PecaModel>((data) => PecaModel.fromJson(data))
+          .toList();
+      if (response.statusCode == 200) {
+        return pecas;
+      } else {
+        Fluttertoast.showToast(msg: jsonDecode(response.body)['message']);
+      }
+    } catch (e) {
+      carregando = false;
     }
   }
 }
--- gpp_app/lib/views/home/home_screen.dart
+++ gpp_app/lib/views/home/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gpp_app/domain/service/PecaService.dart';
+
+import '../../shared/components/TextComponent.dart';
+import '../../shared/components/TitleComponent.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,16 +10,116 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  PecaService service = PecaService();
   @override
   Widget build(BuildContext context) {
     // Your widget tree goes here
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Hello, World!'),
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TitleComponent(
+                      'Peças',
+                    ),
+                  ],
+                ),
+              ),
+              // _buildState()
+            ],
+          ),
+        ),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // CheckboxComponent(),
+            Expanded(
+              child: TextComponent(
+                'ID',
+              ),
+            ),
+            Expanded(
+              child: TextComponent('Número'),
+            ),
+            Expanded(
+              child: TextComponent('Cod. Fabrica'),
+            ),
+            Expanded(
+              child: TextComponent('Descrição'),
+            ),
+            Expanded(
+              child: Text(
+                'Opções',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                // textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        Divider(),
+        service.carregando
+            ? Column(
+                children: [
+                  Container(
+                    height: 400,
+                    child: ListView.builder(
+                      itemCount: service.pecas.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // Padding(padding: EdgeInsets.only(left: 10)),
+                              // CheckboxComponent(),
+                              Expanded(
+                                child: Text(
+                                  service.pecas[index].idPeca.toString(),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16),
+                                  // textAlign: TextAlign.start,
+                                ),
+                              ),
+                              Expanded(
+                                child: Text(
+                                    service.pecas[index].numero.toString()),
+                              ),
+                              // Expanded(
+                              //   child: Text(_pecasController
+                              //               .listaPecas[index].codigo_fabrica ==
+                              //           null
+                              //       ? ''
+                              //       : _pecasController
+                              //           .listaPecas[index].codigo_fabrica
+                              //           .toString()),
+                              // ),
+                              // Expanded(
+                              //   child: Text(_pecasController
+                              //       .listaPecas[index].descricao
+                              //       .toString()),
+                              // ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              )
+            : CircularProgressIndicator(),
+      ],
     );
+  }
+
+  testeRetorno() {
+    //service.lista();
+    print('teste');
   }
 }
