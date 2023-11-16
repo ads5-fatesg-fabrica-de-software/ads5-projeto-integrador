f283602bd34f7754cbcab725479db0299187d8e4
Witor Sather <satherws@gmail.com>
2023-11-16 20:42:49 -0300

    Primeiro commit.

--- gpp_app/lib/main.dart
+++ gpp_app/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
+// teste
 Future<void> main() async {
   await Hive.initFlutter();
   await Hive.openBox('token');
